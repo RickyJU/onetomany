29efefc2c04503d002f9d3f3b386a0a317c3e620
RickyJuvianto <ricky.juvianto98@gmail.com>
2017-09-23 02:52:41 +0700

    Updating Data

--- onetomany/app/Http/routes.php
+++ onetomany/app/Http/routes.php
@@ -31,3 +31,10 @@ Route::get('/read', function(){
 		echo $post->title . "<br/>";
 	}
 });
+
+Route::get('/update', function(){
+	$user = User::find(1);
+	
+	$user->posts()->whereId(1)->update(['title'=>'I Love Laravel So Much', 'body'=>'this is awesome']);
+});
+
